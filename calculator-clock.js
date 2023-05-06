8bc2dd4b9a4bb78ab99a2c1fde9059ea298deffe
timo9939 <timothylau20199@gmail.com>
2023-05-06 10:14:13 +0100

    2 javascript and html file added

---
+++ calculator-clock.js
@@ -0,0 +1,8 @@
+const LosAngles= document.querySelector('#firstCity')
+let London= document.querySelector("#london")
+const HongKong= document.querySelector("#hongKong")
+
+London="hihi"
+
+const time1= LosAngles
+console.log(LosAngles)
